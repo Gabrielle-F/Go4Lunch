9c6d6dd27857e325cbb0866b98d2725b2eb43d45
Gabrielle Freville <gfreville5@gmail.com>
2022-06-16 14:21:01 +0200

    Add AuthenticationActivity and MainActivity

---
+++ app/src/main/java/gabrielle/freville/go4lunch/ui/AuthenticationActivity.java
@@ -0,0 +1,16 @@
+package gabrielle.freville.go4lunch.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import gabrielle.freville.go4lunch.R;
+
+public class AuthenticationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authentication);
+    }
+}
