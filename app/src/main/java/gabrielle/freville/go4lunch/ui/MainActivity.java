37a0850aa9a2f2633411adabe981316963c83527
Gabrielle Freville <gfreville5@gmail.com>
2022-06-16 17:37:45 +0200

    Connect activity_main toolbar with MainActivity

--- app/src/main/java/gabrielle/freville/go4lunch/ui/MainActivity.java
+++ app/src/main/java/gabrielle/freville/go4lunch/ui/MainActivity.java
@@ -1,8 +1,10 @@
 package gabrielle.freville.go4lunch.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 import gabrielle.freville.go4lunch.R;
 
@@ -12,5 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        configureToolbar();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_activity_main_toolbar, menu);
+        return true;
+    }
+
+    private void configureToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.activity_main_toolbar);
+        setSupportActionBar(toolbar);
     }
 }
