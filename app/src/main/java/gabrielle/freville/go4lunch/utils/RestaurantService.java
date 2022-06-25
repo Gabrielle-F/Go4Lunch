95da9599cba95d8a95f148d739adbb276bae9cb4
Gabrielle Freville <gfreville5@gmail.com>
2022-06-25 18:57:34 +0200

    Correction of retrofit builder and nearby search

--- app/src/main/java/gabrielle/freville/go4lunch/utils/RestaurantService.java
+++ app/src/main/java/gabrielle/freville/go4lunch/utils/RestaurantService.java
@@ -9,9 +9,13 @@ import java.util.List;
 import gabrielle.freville.go4lunch.model.Restaurant;
 import gabrielle.freville.go4lunch.model.response.RestaurantResponse;
 import gabrielle.freville.go4lunch.repositories.RestaurantRepository;
+import io.reactivex.Observable;
+import io.reactivex.internal.schedulers.RxThreadFactory;
 import io.reactivex.plugins.RxJavaPlugins;
 import retrofit2.Call;
+import retrofit2.CallAdapter;
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
@@ -19,17 +23,18 @@ import retrofit2.http.Query;
 public interface RestaurantService {
 
     Retrofit retrofit = new Retrofit.Builder()
-            .baseUrl("https://maps.googleapis.com/maps/api/place")
+            .baseUrl("https://maps.googleapis.com/maps/api/place/")
             .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .build();
 
-    @GET("restaurant/{name}")
-    Call<List<RestaurantResponse>> getNearbyRestaurantsList(
+    @GET("nearbysearch/json")
+    Observable<List<RestaurantResponse>> getRestaurants(
             @Query("name") String name,
-            @Query("location") String location,
             @Query("type") String type,
-            @Query("rating") int rating,
-            @Query("user_ratings_total") int userRatingsTotal
+            @Query("geometry") String geometry,
+            @Query("open_now") Boolean opennow,
+            @Query("opening_hours") String openingHours
     );
 
 }
