4dc93f4997b641e785f71ec97aa6e6f94f52ec26
Gabrielle Freville <gfreville5@gmail.com>
2022-07-08 22:52:13 +0200

    Rename RestaurantViewHolder and WorkmatesViewHolder

--- app/src/main/java/gabrielle/freville/go4lunch/ui/RestaurantRecyclerViewAdapter.java
+++ app/src/main/java/gabrielle/freville/go4lunch/ui/RestaurantRecyclerViewAdapter.java
@@ -11,47 +11,46 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.List;
 
 import gabrielle.freville.go4lunch.R;
-import gabrielle.freville.go4lunch.model.NearbyListResponse;
 import gabrielle.freville.go4lunch.model.Restaurant;
-import gabrielle.freville.go4lunch.model.response.RestaurantResponse;
-import gabrielle.freville.go4lunch.utils.RestaurantService;
-import io.reactivex.Observer;
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.disposables.Disposable;
-import io.reactivex.schedulers.Schedulers;
 
-public class RestaurantRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+public class RestaurantRecyclerViewAdapter extends RecyclerView.Adapter<RestaurantRecyclerViewAdapter.RestaurantViewHolder> {
 
-    private RestaurantService restaurantService;
     private List<Restaurant> restaurants;
+    private List<Restaurant> restaurantsList;
 
-
+    public RestaurantRecyclerViewAdapter(List<Restaurant> items){
+        restaurants = items;
+    }
 
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public RestaurantViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.item_restaurant, parent, false);
-        return new ViewHolder(view);
+        return new RestaurantViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(RestaurantViewHolder holder, int position) {
         Restaurant restaurant = restaurants.get(position);
 
+        holder.restaurantName.setText(restaurant.getName());
+        holder.restaurantAddress.setText(restaurant.getAddress());
+        holder.restaurantOpeningHours.setText(restaurant.getHours());
+        holder.restaurantDistance.setText(restaurant.getDistance());
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    public static class RestaurantViewHolder extends RecyclerView.ViewHolder {
 
         TextView restaurantName;
-        TextView restaurantTypeAddress;
+        TextView restaurantAddress;
         TextView restaurantOpeningHours;
         TextView restaurantDistance;
 
-        public ViewHolder(@NonNull View itemView) {
+        public RestaurantViewHolder(@NonNull View itemView) {
             super(itemView);
             restaurantName.findViewById(R.id.item_restaurant_name);
-            restaurantTypeAddress.findViewById(R.id.item_restaurant_address);
+            restaurantAddress.findViewById(R.id.item_restaurant_address);
             restaurantOpeningHours.findViewById(R.id.item_restaurant_open);
             restaurantDistance.findViewById(R.id.item_restaurant_distance);
         }
--- app/src/main/java/gabrielle/freville/go4lunch/ui/WorkmatesRecyclerViewAdapter.java
+++ app/src/main/java/gabrielle/freville/go4lunch/ui/WorkmatesRecyclerViewAdapter.java
@@ -9,31 +9,44 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
 import gabrielle.freville.go4lunch.R;
+import gabrielle.freville.go4lunch.model.User;
+import gabrielle.freville.go4lunch.viewModel.RestaurantViewModel;
 
-public class WorkmatesRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+public class WorkmatesRecyclerViewAdapter extends RecyclerView.Adapter<WorkmatesRecyclerViewAdapter.WorkmatesViewHolder> {
 
+    private List<User> users;
+    private RestaurantViewModel restaurantViewModel;
 
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public WorkmatesViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).
                 inflate(R.layout.item_workmate, parent, false);
-        return new ViewHolder(view);
+        return new WorkmatesViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-
+    public void onBindViewHolder(@NonNull WorkmatesViewHolder holder, int position) {
+        User user = users.get(position);
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    public static class WorkmatesViewHolder extends RecyclerView.ViewHolder {
 
         ImageView workmateProfilePicture;
         TextView workmateRestaurantChoice;
+        TextView workmateName;
+        TextView workmateIsEating;
 
-        public ViewHolder(@NonNull View itemView) {
+        public WorkmatesViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            workmateProfilePicture.findViewById(R.id.item_workmate_profile_picture);
+            workmateName.findViewById(R.id.item_workmate_name);
+            workmateIsEating.findViewById(R.id.item_workmate_is_eating);
+            workmateRestaurantChoice.findViewById(R.id.item_workmate_choice);
         }
     }
 
