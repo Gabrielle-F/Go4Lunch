42a4e2a1a6caf4eb784cea2f0560b802f7d12497
Gabrielle Freville <gfreville5@gmail.com>
2022-07-13 16:35:48 +0200

    Add methods for recovery of users data

--- app/src/main/java/gabrielle/freville/go4lunch/viewModel/UserViewModel.java
+++ app/src/main/java/gabrielle/freville/go4lunch/viewModel/UserViewModel.java
@@ -2,16 +2,27 @@ package gabrielle.freville.go4lunch.viewModel;
 
 import android.content.Context;
 
+import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
+
 import com.google.android.gms.tasks.Continuation;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentSnapshot;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import gabrielle.freville.go4lunch.model.SelectedRestaurant;
 import gabrielle.freville.go4lunch.model.User;
 import gabrielle.freville.go4lunch.repositories.UserRepository;
 
-public class UserViewModel {
+public class UserViewModel extends androidx.lifecycle.ViewModel {
 
     private UserRepository userRepository;
+
+    public UserViewModel(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     public void createUser(){
         userRepository.createUser();
@@ -20,6 +31,14 @@ public class UserViewModel {
     public Task<User> getUserData() {
         return userRepository.getUserData().continueWith
                 ((Continuation<DocumentSnapshot, User>) task -> task.getResult().toObject(User.class));
+    }
+
+    public Task<String> getFirstName(String firstName) {
+        return userRepository.getFirstName(firstName).continueWith((Continuation<DocumentSnapshot, String>) task -> task.getResult().toString());
+    }
+
+    public Task<String> getEmail(String email) {
+        return userRepository.getEmail(email).continueWith((Continuation<DocumentSnapshot, String>) task -> task.getResult().toString());
     }
 
     public Task<Void> deleteUser(Context context) {
