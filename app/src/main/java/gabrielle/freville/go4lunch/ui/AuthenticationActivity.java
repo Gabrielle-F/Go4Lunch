4320c2ac489690eb39547131de8cf6937cb35f79
Gabrielle Freville <gfreville5@gmail.com>
2022-07-17 17:27:18 +0200

    Implement google authentication

--- app/src/main/java/gabrielle/freville/go4lunch/ui/AuthenticationActivity.java
+++ app/src/main/java/gabrielle/freville/go4lunch/ui/AuthenticationActivity.java
@@ -1,6 +1,9 @@
 package gabrielle.freville.go4lunch.ui;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,14 +12,33 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Collections;
+import java.util.List;
+
 import gabrielle.freville.go4lunch.R;
+import gabrielle.freville.go4lunch.injections.Injection;
+import gabrielle.freville.go4lunch.injections.ViewModelFactory;
+import gabrielle.freville.go4lunch.viewModel.UserViewModel;
 
 public class AuthenticationActivity extends AppCompatActivity {
 
+    private ImageView background;
     private ImageView logo;
     private TextView appPresentation;
     private Button facebookButton;
     private Button googleButton;
+    private List<AuthUI.IdpConfig> googleProvider = Collections.singletonList(new AuthUI.IdpConfig.GoogleBuilder().build());
+    private UserViewModel userViewModel;
+    private View snackBarContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +50,15 @@ public class AuthenticationActivity extends AppCompatActivity {
         facebookButton = findViewById(R.id.authentication_activity_facebook_button);
         googleButton = findViewById(R.id.authentication_activity_google_button);
 
+        snackBarContainer = this.findViewById(R.id.authentication_constraint_layout);
+
+        configureUserViewModel();
         configureListeners();
+    }
+
+    private void configureUserViewModel() {
+        ViewModelFactory viewModelFactory = Injection.provideViewModelFactory(this);
+        this.userViewModel = new ViewModelProvider(this, viewModelFactory).get(UserViewModel.class);
     }
 
     private void configureListeners() {
@@ -38,10 +68,60 @@ public class AuthenticationActivity extends AppCompatActivity {
                 startMainActivity();
             }
         });
+
+        googleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startSignInActivity();
+            }
+        });
+    }
+
+    private void startSignInActivity() {
+                Intent signInIntent = AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setTheme(R.style.LoginTheme)
+                        .setAvailableProviders(googleProvider)
+                        .setIsSmartLockEnabled(false, true)
+                        .build();
+                signInLauncher.launch(signInIntent);
+    }
+
+    private final ActivityResultLauncher<Intent> signInLauncher = registerForActivityResult(
+            new FirebaseAuthUIActivityResultContract(),
+            new ActivityResultCallback<FirebaseAuthUIAuthenticationResult>() {
+                @Override
+                public void onActivityResult(FirebaseAuthUIAuthenticationResult result) {
+                    onSignInResult(result);
+                }
+            }
+    );
+
+    private void showSnackBar(String message) {
+        Snackbar.make(snackBarContainer, message, Snackbar.LENGTH_SHORT).show();
+    }
+
+    private void onSignInResult(FirebaseAuthUIAuthenticationResult result) {
+        IdpResponse response = result.getIdpResponse();
+        if (result.getResultCode() == RESULT_OK) {
+            userViewModel.createUser();
+            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+            showSnackBar(getString(R.string.connection_succeed));
+        } else {
+            if (response.getError()!= null) {
+                showSnackBar(getString(R.string.error_authentication_canceled));
+            } else if (result.getResultCode() == ErrorCodes.NO_NETWORK) {
+                showSnackBar(getString(R.string.error_no_internet));
+            } else if (result.getResultCode() == ErrorCodes.UNKNOWN_ERROR) {
+                showSnackBar(getString(R.string.unknown_error));
+            }
+        }
     }
 
     private void startMainActivity() {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
+
+
 }
